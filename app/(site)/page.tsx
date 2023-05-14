14784103eb33a93b3125020eab1ae3d417507e9f
Pavel <pavlozherebnov@gmail.com>
2023-05-15 00:34:08 +0300

    Auth setup

---
+++ app/(site)/page.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+export default function Home() {
+   return (
+      <div className='flex flex-col min-h-full justify-center py-12 sm: px-6 bg-gray-100'>
+         <div className='sm:mx-auto sm: w-full sm:max-w-md'>
+            <Image
+               alt='Logo'
+               height={48}
+               width={48}
+               className='mx-auto w-auto'
+               src='/images/logo.png'
+            />
+            <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>
+               Sing in to your account
+            </h2>
+         </div>
+         {/* <AuthForm /> */}
+      </div>
+   );
+}
