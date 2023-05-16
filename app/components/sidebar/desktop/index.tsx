40ac3c3b7a23744ab8021a5365224ba68402fff6
Pavel <pavlozherebnov@gmail.com>
2023-05-16 15:14:21 +0300

    fetch current user and Avatar component

--- app/components/sidebar/desktop/index.tsx
+++ app/components/sidebar/desktop/index.tsx
@@ -3,10 +3,18 @@
 import { useState } from 'react';
 import { useRoutes } from '@/app/hooks';
 import DesktopItem from './item';
+import Avatar from '../../avatar';
+import { User } from '@prisma/client';
 
-export default function DesktopSidebar() {
+interface Props {
+   user: User;
+}
+
+export default function DesktopSidebar({ user }: Props) {
    const routes = useRoutes();
    const [isOpen, setIsOpen] = useState(false);
+
+   console.log(user);
    return (
       <div className='hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between'>
          <nav className='mt-4 flex flex-col justify-between'>
@@ -15,6 +23,14 @@ export default function DesktopSidebar() {
                   <DesktopItem key={item.label} {...item} />
                ))}
             </ul>
+         </nav>
+         <nav className='mt-4 flex flex-col items-center justify-between'>
+            <div
+               onClick={() => setIsOpen(true)}
+               className='cursor-pointer hover:opacity-75 transition'
+            >
+               <Avatar user={user} />
+            </div>
          </nav>
       </div>
    );
--- app/components/sidebar/index.tsx
+++ app/components/sidebar/index.tsx
@@ -1,14 +1,17 @@
 import DesktopSidebar from './desktop';
 import MobileFooter from './mobile';
 
+import getCurrentUser from '@/app/actions/getCurrentUser';
+
 interface Props {
    children: React.ReactNode;
 }
 
 export default async function SideBar({ children }: Props) {
+   const currentUser = await getCurrentUser();
    return (
       <div className='h-full'>
-         <DesktopSidebar />
+         <DesktopSidebar user={currentUser!} />
          <MobileFooter />
          <main className='lg:pl-20 h-full'>{children}</main>
       </div>
